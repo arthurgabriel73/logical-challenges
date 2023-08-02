bf813995df04357021395ae47707bfc30b94b386
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-02 20:37:43 -0300

    feat: implements palindrome program

--- challenges/04_palindrome/checkPalindrome.ts
+++ challenges/04_palindrome/checkPalindrome.ts
@@ -1,10 +1,11 @@
-export function checkPalindrome(word: string): boolean {
-
+export function checkPalindrome(word: string): string {
   const sanitizedWord = word.toLowerCase().replace(/[^a-z]/, '');
 
   for (let i = 0, j = sanitizedWord.length - 1; i <= j; i++, j--) {
-    if (sanitizedWord.charAt(i) !== sanitizedWord.charAt(j)) return false;
+    if (sanitizedWord.charAt(i) !== sanitizedWord.charAt(j)) {
+      return `The word ${word} is not a palindrome!`;
+    }
   }
   
-  return true;  
+  return `The word ${word} is a palindrome!`;  
 }
