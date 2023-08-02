81bb25cd5bcae90a0a9b4b5d680c2adbc09dde13
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-01 22:20:51 -0300

    test: implements tests for subtraction operations

--- simpleCalculator/test/Calculator.spec.ts
+++ simpleCalculator/test/Calculator.spec.ts
@@ -2,12 +2,22 @@ import { Calculator } from "../src/Calculator"
 import { Operator } from "../src/Operator";
 
 type Operation = [number, number, Operator, number];
+
 const additionCases: Operation[] = [[1, 2, '+', 3], [-1, -2, '+', -3], [-10, 12, '+', 2], [3, -12, '+', -9]]
+const subtractionCases: Operation[] = [[10, 10, '-', 0], [10, 11, '-', -1], [12, 10, '-', 2], [-10, -10, '-', 0],]
 
 describe('simpleCalculator test', () => {
   const calculator = new Calculator();
 
   it.each(additionCases)('should test calculator addition', (firstNumber, secondNumber, operator, expectedResult) => {
+    // Arrange & Act
+    const result = calculator.execute({firstNumber, secondNumber, operator})
+
+    // Assert
+    expect(result).toEqual(expectedResult)
+  })
+
+  it.each(subtractionCases)('should test calculator subtraction', (firstNumber, secondNumber, operator, expectedResult) => {
     // Arrange & Act
     const result = calculator.execute({firstNumber, secondNumber, operator})
 
