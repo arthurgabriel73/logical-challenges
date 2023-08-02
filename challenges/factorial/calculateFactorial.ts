d0f4855fa1212beb6bbf7295a3f0d52f0f0fe164
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-02 19:49:07 -0300

    feat: implements factorial function

--- challenges/factorial/calculateFactorial.ts
+++ challenges/factorial/calculateFactorial.ts
@@ -3,7 +3,7 @@ export function getRecursiveFactorial(num: number): number {
   return num * getRecursiveFactorial(num - 1);
 }
 
-export function getFactorial(num: number): number {
+export function getFactorialForBigNumbers(num: number): number {
   let factorial = 1;
   for (let i = 2; i <= num; i++) {
     factorial *= i;
