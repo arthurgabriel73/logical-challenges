e1d5de6793c7bbda923199c7910fbcbcb7425281
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-01 22:33:24 -0300

    test: implements tests for division operation

--- simpleCalculator/test/Calculator.spec.ts
+++ simpleCalculator/test/Calculator.spec.ts
@@ -6,6 +6,7 @@ type Operation = [number, number, Operator, number];
 const additionCases: Operation[] = [[1, 2, '+', 3], [-1, -2, '+', -3], [-10, 12, '+', 2], [3, -12, '+', -9]]
 const subtractionCases: Operation[] = [[10, 10, '-', 0], [10, 11, '-', -1], [12, 10, '-', 2], [-10, -10, '-', 0],]
 const multiplicationCases: Operation[] = [[1, 10, '*', 10], [10, -1, '*', -10], [12, 10, '*', 120], [-10, -10, '*', 100],]
+const disivionCases: Operation[] = [[1, 10, '/', 0.1], [10, -1, '/', -10], [12, 10, '/', 1.2], [-10, -10, '/', 1],]
 
 describe('simpleCalculator test', () => {
   const calculator = new Calculator();
@@ -27,6 +28,14 @@ describe('simpleCalculator test', () => {
   })
 
   it.each(multiplicationCases)('should test calculator multiplication', (firstNumber, secondNumber, operator, expectedResult) => {
+    // Arrange & Act
+    const result = calculator.execute({firstNumber, secondNumber, operator})
+
+    // Assert
+    expect(result).toEqual(expectedResult)
+  })
+
+  it.each(disivionCases)('should test calculator disivion', (firstNumber, secondNumber, operator, expectedResult) => {
     // Arrange & Act
     const result = calculator.execute({firstNumber, secondNumber, operator})
 
