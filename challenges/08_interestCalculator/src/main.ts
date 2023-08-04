1a56f58aac7486594a0392378af8853ed598b8a6
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-08-03 23:45:15 -0300

    fix: types input handler implementation

--- challenges/08_interestCalculator/src/main.ts
+++ challenges/08_interestCalculator/src/main.ts
@@ -1,11 +1,12 @@
 import { InterestCalculator } from "./InterestCalculator";
 import { Processor } from "./Processor";
 import { InputHandler } from "./inputHandler";
+import { IInputHandler } from "./protocols/IInputHandler";
 import { IInterestCalculator } from "./protocols/IInterestCalculator";
 
 async function main() {
   const interestCalculator: IInterestCalculator = new InterestCalculator();
-  const inputHandler = new InputHandler();
+  const inputHandler: IInputHandler = new InputHandler();
   const processor = new Processor(inputHandler, interestCalculator);
   processor.process();
 }
